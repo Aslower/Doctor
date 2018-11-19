12c8de22b555a695e8b21c72423e1e75017c322d
aslower <yeyuwei@m.scnu.edu.cn>
2018-11-19 20:39:49 +0800

    Add splash_activity

---
+++ app/src/main/java/com/erving/doctor/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.erving.doctor
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SplashActivity:AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+
+    }
+
+}
